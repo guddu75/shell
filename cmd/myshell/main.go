99f6492fed7b3816c517cca1240489ac1b46537d
Debopriyo <guddumajumder75@gmail.com>
2024-06-01 20:16:23 +0530

    REPL 01

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -13,13 +13,17 @@ func main() {
 	// Uncomment this block to pass the first stage
 	fmt.Fprint(os.Stdout, "$ ")
 
+	for {
+		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+		// log.Printf("input", input)
+
+		output := input[:len(input)-1] + ": command not found\n"
+
+		fmt.Fprint(os.Stdout, output)
+
+	}
+
 	// Wait for user input
-	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-
-	// log.Printf("input", input)
-
-	output := input[:len(input)-1] + ": command not found\n"
-
-	fmt.Fprint(os.Stdout, output)
 
 }
